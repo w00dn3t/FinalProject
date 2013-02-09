121015c802efe72abcaf8d6cc8d1851f591ccbae
Raayan Pillai <raayanp01@gmail.com>
2013-02-08 22:46:29 -0500

    Uploaded 2 files

---
+++ LoginUI.java
@@ -0,0 +1,55 @@
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.swing.*;
+
+public class LoginUI {
+  	JFrame frameMain = new JFrame("Login");
+
+	//Create Elements
+	JLabel usernameLabel = new JLabel("Username");
+	JTextField usernameTextField = new JTextField(20);
+
+	JLabel passwordLabel = new JLabel("Password");
+	JTextField passwordTextField = new JTextField(20);
+	
+	JButton loginButton = new JButton("Login");
+	public LoginUI(final PrintWriter out) {
+		GridBagLayout layout = new GridBagLayout();
+		GridBagConstraints c = new GridBagConstraints();
+		frameMain.setSize(350, 150);
+		frameMain.setVisible(true);
+		frameMain.setLayout(layout);
+		
+		//Insert Elements
+		c.gridx = 0; c.gridy = 0;
+		c.fill = GridBagConstraints.HORIZONTAL;
+		frameMain.add(usernameLabel, c);
+		
+		c.gridx = 1; c.gridy = 0;
+		frameMain.add(usernameTextField, c);
+
+		c.gridx = 0; c.gridy = 1;
+		frameMain.add(passwordLabel, c);
+
+		c.gridx = 1; c.gridy = 1;
+		frameMain.add(passwordTextField, c);
+
+		c.gridwidth = 2;
+		c.gridx = 0; c.gridy = 2;
+		frameMain.add(loginButton, c);
+		//End Insert Elements
+		
+		//Function for button
+		loginButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				out.println(usernameTextField.getText());
+				out.println(passwordTextField.getText());
+			}
+		});
+
+	}
+}
---
+++ SystemMain.java
@@ -0,0 +1,32 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.Socket;
+import java.net.UnknownHostException;
+
+public class SystemMain {
+	public static void main(String[] args){
+		Socket sock = null;
+        PrintWriter out = null;
+        BufferedReader in = null;
+ 
+        try {
+            out = new PrintWriter(sock.getOutputStream(), true);
+            in = new BufferedReader(new InputStreamReader(sock.getInputStream()));
+            System.err.println("Successful connect!");
+        } catch (UnknownHostException e) {
+            System.err.println("Couldn't connect to horsetamer.no-ip.org on port 7777");
+            System.exit(1);
+        } catch (IOException e) {
+            System.err.println("Couldn't get I/O for the connection to: taranis.");
+            System.exit(1);
+        }
+        
+        LoginUI loginScr = new LoginUI(out);
+        
+        
+        
+	}
+
+}
